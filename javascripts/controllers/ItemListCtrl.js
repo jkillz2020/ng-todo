249ed44e113b30bf4fdde45033a1e46a0ec05706
jkillz2020 <kilgore.josh@gmail.com>
2016-11-19 11:38:13 -0600

    add route ctrl

--- javascripts/controllers/ItemListCtrl.js
+++ javascripts/controllers/ItemListCtrl.js
@@ -1,4 +1,30 @@
 "use strict";
-app.controller("ItemListCtrl", function($scope){
-  $scope.message="itemListCtrl"
+app.controller("ItemListCtrl", function($scope, ItemFactory){
+  $scope.items = [];
+
+  let getItems = function(){
+    ItemFactory.getItemList().then(function(fbItems){
+      $scope.items=fbItems;
+  })
+}
+
+getItems();
+
+
+  $scope.addNewItem = function(){
+    $scope.newTask.isCompleted=false;
+    ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+    getItems();
+    $scope.newTask= {};
+    $scope.showListView = true;
+    })
+  }
+
+  $scope.deleteItem = function(itemId){
+    console.log("you deleted item", itemId);
+    ItemFactory.deleteItem(itemId).then(function(response){
+      console.log("here now", response)
+      getItems();
+    })
+  }
 })
--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,4 +1,12 @@
 "use strict";
-app.controller("ItemNewCtrl", function($scope){
-  $scope.message="itemNewCtrl"
+app.controller("ItemNewCtrl", function($scope, $location, ItemFactory){
+  $scope.newTask = {};
+
+  $scope.addNewItem = function(){
+    $scope.newTask.isCompleted=false;
+    ItemFactory.postNewItem($scope.newTask).then(function(itemId){
+      $location.url("/items/list");
+    $scope.newTask= {};
+    })
+  }
 })
